05713d08c9ed2bbeba5ce71ffcfea92ffa57d62e
dovezukatmn <dovezuka.tmn@ya.ru>
2026-02-18 20:58:19 +0500

    fix: restore laravel/ui in composer.json and cleanup User model

--- admin/app/Models/User.php
+++ admin/app/Models/User.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
